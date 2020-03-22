7c015c1ab418d95d86d613637e106c7bae2e3ac5
Gary Sentosa <gary.sentosa@gmail.com>
2020-03-22 20:45:36 +0900

    Add user table migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(class_config=Config):
     app = Flask(__name__)
     app.config.from_object(class_config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
@@ -14,3 +22,6 @@ def create_app(class_config=Config):
     app.register_blueprint(auth_bp)
 
     return app
+
+
+from app import models
