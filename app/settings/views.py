33e24059f1eee860735c056645ad0522625dafee
Gary Sentosa <gary.sentosa@gmail.com>
2020-03-27 02:31:33 +0900

    Add drop-down list with GPIO pins as options

--- app/settings/views.py
+++ app/settings/views.py
@@ -2,12 +2,16 @@ from flask import render_template
 from flask_login import login_required
 
 from app.settings import bp
+from app.settings.forms import PyTenkiForm
 
 
 @bp.route('/settings/pytenki')
 @login_required
 def pytenki():
+    form = PyTenkiForm()
+
     return render_template(
         'settings/pytenki.html',
-        title='PyTenki - Settings'
+        title='PyTenki - Settings',
+        form=form
     )
