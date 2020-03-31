7b72bc1153e0779e437ef77d2bbd00ddbf5b6ad9
Gary Sentosa <gary.sentosa@gmail.com>
2020-03-31 23:16:56 +0900

    Use loop to assert elements against response data

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -6,9 +6,9 @@ def test_fetch_login_page(client):
     response = client.get(url_for('auth.login'))
 
     assert response.status_code == 200
-    assert b'Username' in response.data
-    assert b'Password' in response.data
-    assert b'Log In' in response.data
+    elements = (b'Username', b'Password', b'Log In')
+    for element in elements:
+        assert element in response.data
 
 
 def test_invalid_user_login(client, app_db):
@@ -26,8 +26,9 @@ def test_user_login(login_client):
     assert response.status_code == 200
     assert request.path == url_for('main.index')
     assert not current_user.is_anonymous
-    assert b'Settings' in response.data
-    assert b'Log Out' in response.data
+    elements = (b'Settings', b'Log Out')
+    for element in elements:
+        assert element in response.data
 
 
 def test_user_logout(client, login_client):
