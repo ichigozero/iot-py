22a5eaf8a1d959783496eb117a53de24ddc092ae
Gary Sentosa <gary.sentosa@gmail.com>
2020-08-19 16:59:50 +0900

    Add packaging information

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='iot-py',
+    version='0.0.1',
+    description='IoT web app built with Flask for Raspberry Pi',
+    license='MIT',
+    author_email='gary.sentosa@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask>=1.1.1',
+        'Flask-Login>=0.5.0',
+        'Flask-Migrate>=2.5.3',
+        'Flask-SQLAlchemy>=2.4.1',
+        'Flask-SSE>=0.2.1',
+        'Flask-WTF>=0.14.3',
+        'gevent>=20.4.0',
+        'gunicorn>=20.0.4',
+        'pydensha>=1.0.0',
+        'pytenki>=1.0.0',
+        'python-dotenv>=0.12.0',
+        'redis>=3.4.1',
+        'tenkihaxjp>=1.0.0',
+        'traininfojp>=1.0.0',
+    ],
+)
